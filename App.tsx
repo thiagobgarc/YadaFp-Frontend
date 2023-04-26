a744424e16ffcead5cd2a0e6aee6d60e401ec9c7
Thiago Bueno Garcia <Thiagobuenogarcia1@gmail.com>
2023-04-25 21:14:21 -0400

    ADDED - Styling - logic

---
+++ App.tsx
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { Home } from './src/screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Yada } from './src/components/Yada';
+import { MainPage } from './src/components/MainPage';
+import { FoodPantry } from './src/components/FoodPantry';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+const Tab = createBottomTabNavigator()
+function App(): JSX.Element {
+   const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    ...styles.screen,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{flexGrow: 1}}
+        style={backgroundStyle}>
+          <NavigationContainer>
+            <Tab.Navigator initialRouteName='Home'>
+              <Tab.Screen name='Yada' component={Yada}/>
+              <Tab.Screen name='Home' component={MainPage}/>
+              <Tab.Screen name='Food Pantry' component={FoodPantry}/>
+            </Tab.Navigator>
+          </NavigationContainer>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  },
+  text: {
+    color: 'black',
+  }
+});
+
+export default App;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,76 @@
+import { useCallback, useEffect, useState } from "react"
+import { Text, Image, View, StyleSheet, ScrollView } from "react-native"
+import { Toolbar } from "./Toolbar"
+import { About } from "./About"
+
+const URL = "https://yadafb-backend.herokuapp.com/yadafp/"
+
+export const MainPage = () => {
+    const [form, setForm] = useState({})
+
+    const getForm = useCallback (async () => {
+        const form = await fetch(`${URL}`)
+        const data = await form.json()
+        setForm(data)
+    }, [])
+
+    useEffect(() => {
+        getForm()
+    }, [])
+
+    return (
+        <ScrollView style={styles.color}>
+            <Text style={styles.title}>
+                Welcome to 
+            </Text>
+            <Image source={{uri: 'https://assembly-of-god-world-vision-ministry-youth.square.site/uploads/b/6d194390-5b84-11ea-b829-138bd9785118/902c45649a4729c9831f8b07a722e7f5.png'}} style={styles.image}/>
+            <Text style={styles.text}>
+                AGWV
+            </Text>
+            <Text style={styles.text2}>
+                Mobile
+            </Text>
+            <About style={styles.about}/>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 150,
+        left: '25%',
+        top: '20%',
+        marginBottom: '100%'
+    },
+    text: {
+        fontSize: 50,
+        textAlign: 'center',
+        color:'black',
+        bottom: '20%',
+    },
+    title: {
+        fontSize: 50,
+        textAlign: 'center',
+        top: '15%',
+        color:'black'
+    },
+    text2: {
+        fontSize: 35,
+        textAlign: 'center',
+        color:'black',
+        bottom: '20%',
+    },
+    color: {
+        backgroundColor: 'linen',
+        flex: .5,
+        // border: 'solid',
+        // borderColor: 'black',
+        // borderWidth: 2,
+    },
+    about: {
+        color: 'black',
+        bottom: 200
+    }
+})
